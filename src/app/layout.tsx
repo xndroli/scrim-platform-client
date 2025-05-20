e427bd4f1746a5db6aef40dad2288977799e4c1e
xndroli <103154250+xndroli@users.noreply.github.com>
2025-05-19 20:47:27 -0400

    fixed font ref in app/layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,30 +2,35 @@ import type { Metadata } from "next";
 import "../styles/globals.css";
 import { Toaster } from "../components/ui/sonner";
 
-import localFont from "next/font/local";
-import { ReactNode } from "react";
+//import localFont from "next/font/local";
+import type { ReactNode } from "react";
 import { Providers } from "./providers";
 
+import { zentry } from '@/lib/fonts';
+import { general } from '@/lib/fonts';
+import { circular } from '@/lib/fonts';
+import { robert } from '@/lib/fonts';
+
 // Local Fonts
-const zentry = localFont({
-  src: "../../public/fonts/zentry-regular.woff2",
-  variable: "--font-zentry",
-});
+// const zentry = localFont({
+//   src: "../../public/fonts/zentry-regular.woff2",
+//   variable: "--font-zentry",
+// });
 
-const general = localFont({
-  src: "../../public/fonts/general.woff2",
-  variable: "--font-general",
-});
+// const general = localFont({
+//   src: "../../public/fonts/general.woff2",
+//   variable: "--font-general",
+// });
 
-const circular = localFont({
-  src: "../../public/fonts/circularweb-book.woff2",
-  variable: "--font-circular-web",
-});
+// const circular = localFont({
+//   src: "../../public/fonts/circularweb-book.woff2",
+//   variable: "--font-circular-web",
+// });
 
-const robert = localFont({
-  src: "../../public/fonts/robert-regular.woff2",
-  variable: "--font-robert-regular",
-});
+// const robert = localFont({
+//   src: "../../public/fonts/robert-regular.woff2",
+//   variable: "--font-robert-regular",
+// });
 
 export const metadata: Metadata = {
   title: "Raijin Ascendancy E-Sports",
