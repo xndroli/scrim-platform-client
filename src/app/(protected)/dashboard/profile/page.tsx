1438a6b80926c2a02efd063820e11ac22bf605e4
xndroli <103154250+xndroli@users.noreply.github.com>
2025-05-20 09:07:32 -0400

    remove db ref from scrim page

--- src/app/(protected)/dashboard/profile/page.tsx
+++ src/app/(protected)/dashboard/profile/page.tsx
@@ -1,16 +1,84 @@
 import { Button } from '@/components/ui/button';
-import { signOut } from '@/auth';
+import { useAuth } from '../../../../lib/auth/AuthContext';
 import React from 'react';
-import { sampleEvents } from '@/constants';
 import EventList from '@/components/EventList';
 
+const sampleEvents = [
+    {
+        id: 1,
+        title: "The Midnight Battle",
+        creator: "Raijin",
+        game: "Apex Legends",
+        rating: 4.6,
+        total_slots: 20,
+        available_slots: 10,
+        description:
+            "A dazzling novel about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+        color: "#1c1f40",
+        cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+        video: "/sample-video.mp4?updatedAt=1722593504152",
+        summary:
+            "A dazzling event about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+    },
+    {
+        id: 2,
+        title: "The Morning Battle",
+        creator: "Raijin",
+        game: "Apex Legends",
+        rating: 4.6,
+        total_slots: 20,
+        available_slots: 10,
+        description:
+            "A dazzling novel about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+        color: "#1c1f40",
+        cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+        video: "/sample-video.mp4?updatedAt=1722593504152",
+        summary:
+            "A dazzling event about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+    },
+    {
+        id: 3,
+        title: "The Dawn Battle",
+        creator: "Raijin",
+        game: "Apex Legends",
+        rating: 4.6,
+        total_slots: 20,
+        available_slots: 10,
+        description:
+            "A dazzling novel about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+        color: "#1c1f40",
+        cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+        video: "/sample-video.mp4?updatedAt=1722593504152",
+        summary:
+            "A dazzling event about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+    },
+    {
+        id: 4,
+        title: "The Dusk Battle",
+        creator: "Raijin",
+        game: "Apex Legends",
+        rating: 4.6,
+        total_slots: 20,
+        available_slots: 10,
+        description:
+            "A dazzling novel about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+        color: "#1c1f40",
+        cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+        video: "/sample-video.mp4?updatedAt=1722593504152",
+        summary:
+            "A dazzling event about all the choices that go into a life well lived, The Midnight Battle tells the story of ready player one as they find themselves between life and death.",
+    },
+];
+
 const Page = () => {
+    const { logout } = useAuth();
+
     return (
         <>
             <form action={async () => {
                 'use server';
 
-                await signOut();
+                await logout();
                 }}
                 className="mb-10"
             >
