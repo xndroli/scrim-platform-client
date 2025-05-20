396335e40a29bf2414addc13353b5957d4a3fbd2
xndroli <103154250+xndroli@users.noreply.github.com>
2025-05-20 10:22:19 -0400

    corrected path to globals css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "@/styles/globals.css";
+import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 import localFont from "next/font/local";
